1da925c08251b6780d31d232d72e32c1258da1e1
Granth Gharewal <gharewal12@gmail.com>
2023-01-21 22:10:22 +0530

    Implemented Array functionality with following methods i. Insert() ii. RemoveAt() iii, IndexOf() iv. Print()

---
+++ DataStructures/ArrayDs.cs
@@ -0,0 +1,75 @@
+﻿namespace DataStructures
+{
+    public class ArrayDs<T>
+        where T : struct
+    {
+        private T[] _array;
+        private int _count = 0;
+
+        /// <summary>
+        /// Define length of array at object initialization
+        /// </summary>
+        /// <param name="length"></param>
+        public ArrayDs(int length)
+        {
+            _array = new T[length];
+        }
+
+        /// <summary>
+        /// Insert a new item into an array
+        /// </summary>
+        /// <param name="item"></param>
+        public void Insert(T item)
+        {
+            if (_count == _array.Length) {
+                var oldArray = _array;
+                _array = new T[_count * 2];
+                for (int i = 0; i < _count; i++) {
+                    _array[i] = oldArray[i];
+                }
+            }
+            _array[_count++] = item;
+        }
+
+        /// <summary>
+        /// Remove element from specific index of array
+        /// </summary>
+        /// <param name="index"></param>
+        /// <exception cref="Exception"></exception>
+        public void RemoveAt(int index)
+        {
+            if (index >= _count || index < 0) {
+                throw new Exception("Index out of range");
+            }
+            for (int y = index; y < _count; y++) {
+                _array[y] = _array[y + 1];
+            }
+            _count--;
+        }
+
+        /// <summary>
+        /// Get data from specific index of array
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        public int IndexOf(T item)
+        {
+            for (int u = 0; u < _count; u++) {
+                if (_array[u].ToString() == item.ToString()) {
+                    return u;
+                }
+            }
+            return -1;
+        }
+
+        /// <summary>
+        /// Print all the elements of array
+        /// </summary>
+        public void Print()
+        {
+            for (int i = 0; i < _count; i++) {
+                Console.WriteLine(_array[i]);
+            }
+        }
+    }
+}
--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DataStructures;
+
+Console.WriteLine("Hello, Welcome to Data Structures!");
+
+/* Arrays */
+#region
+var numbers = new ArrayDs<int>(3);
+numbers.Insert(10);
+numbers.Insert(20);
+numbers.Insert(30);
+numbers.Insert(40);
+Console.WriteLine("Index of 30 = " + numbers.IndexOf(30));
+numbers.RemoveAt(3);
+numbers.Print();
+#endregion

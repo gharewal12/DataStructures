abeafa9c66f2da949a665548b22c8c1992489529
Granth Gharewal <gharewal12@gmail.com>
2023-01-22 17:11:53 +0530

    Added following extension methods for ArrayDs class: i. Max() ii. Intersect() iii. Reverse() iv. InsertAt()

--- DataStructures/ArrayDs.cs
+++ DataStructures/ArrayDs.cs
@@ -71,5 +71,83 @@
                 Console.WriteLine(_array[i]);
             }
         }
+
+        /// <summary>
+        /// Get maximum value
+        /// </summary>
+        /// <returns></returns>
+        public T Max()
+        {
+            T max = default;
+
+            for (int i = 0; i < _array.Length; i++) {
+                if (Convert.ToInt32(_array[i].ToString()) > Convert.ToInt32(max.ToString())) {
+                    max = _array[i];
+                }
+            }
+
+            return max;
+        }
+
+        /// <summary>
+        /// Get common elements between 2 arrays
+        /// </summary>
+        /// <param name="array2"></param>
+        /// <returns></returns>
+        public ArrayDs<T> Intersect(ArrayDs<T> array2)
+        {
+            var result = new ArrayDs<T>(array2._count > _array.Length ? _array.Length : array2._count);
+            for (int i = 0; i < _array.Length; i++) {
+                for (int j = 0; j < array2._count; j++) {
+                    if (_array[i].ToString() == array2._array[j].ToString()) {
+                        result.Insert(_array[i]);
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Reverse the elements of array
+        /// </summary>
+        public void Reverse()
+        {
+            var temp = new T[_array.Length];
+
+            for (int i = 0; i < _count; i++) {
+                temp[i] = _array[_count - i - 1];
+            }
+
+            _array = temp;
+        }
+
+        /// <summary>
+        /// Insert element at specified index
+        /// </summary>
+        /// <param name="index"></param>
+        /// <param name="item"></param>
+        /// <exception cref="Exception"></exception>
+        public void InsertAt(int index, T item)
+        {
+            if (index > _array.Length) {
+                throw new Exception("Index out of range");
+            }
+
+            if (index > _count) {
+                index = _count;
+            }
+
+            var newArray = new T[_count + 1];
+            for (int i = 0; i < index; i++) {
+                newArray[i] = _array[i];
+            }
+            newArray[index] = item;
+            for (int i = index + 1; i < _count + 1; i++) {
+                newArray[i] = _array[i - 1];
+            }
+            _array = newArray;
+            _count++;
+        }
     }
 }
--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -1,15 +1,28 @@
 ﻿using DataStructures;
+using System.ComponentModel.DataAnnotations;
 
 Console.WriteLine("Hello, Welcome to Data Structures!");
 
 /* Arrays */
 #region
-var numbers = new ArrayDs<int>(3);
+var numbers = new ArrayDs<int>(4);
 numbers.Insert(10);
 numbers.Insert(20);
 numbers.Insert(30);
 numbers.Insert(40);
+numbers.Insert(50);
 Console.WriteLine("Index of 30 = " + numbers.IndexOf(30));
 numbers.RemoveAt(3);
+Console.WriteLine("Max = " + numbers.Max());
+var array2 = new ArrayDs<int>(3);
+array2.Insert(20);
+array2.Insert(30);
+array2.Insert(50);
+var intersection = numbers.Intersect(array2);
+Console.WriteLine("Intersection");
+intersection.Print();
+Console.WriteLine("Array 1");
+numbers.Reverse();
+numbers.InsertAt(5, 70);
 numbers.Print();
 #endregion

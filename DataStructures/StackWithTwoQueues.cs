dda1068655cbcc8f511fd411150a2ee345c89b65
Granth Gharewal <gharewal12@gmail.com>
2023-02-09 14:13:45 +0530

    Implemented stack using two queues

---
+++ DataStructures/StackWithTwoQueues.cs
@@ -0,0 +1,36 @@
+﻿namespace DataStructures
+{
+    public class StackWithTwoQueues
+    {
+
+        private Queue<int> q1;
+        private Queue<int> q2;
+
+        public StackWithTwoQueues()
+        {
+            q1 = new Queue<int>();
+            q2 = new Queue<int>();
+        }
+
+        public void Push(int x)
+        {
+            q2.Enqueue(x);
+
+            while (q1.Count > 0) {
+                q2.Enqueue(q1.Dequeue());
+            }
+
+            var q = q1;
+            q1 = q2;
+            q2 = q;
+        }
+
+        public int Pop()
+        {
+            if (q1.Count == 0) {
+                return -1;
+            }
+            return q1.Dequeue();
+        }
+    }
+}
